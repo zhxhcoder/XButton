579479a8c6e06b5c918322124da1e404df236991
zhxh <zhxhcoder@gmail.com>
2018-06-28 19:23:16 +0800

    动画效果version 2.1

--- app/src/main/java/com/zhxh/xbutton/MainActivity.java
+++ app/src/main/java/com/zhxh/xbutton/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zhxh.xbutton;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -15,11 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
         XButton button1 = findViewById(R.id.XButton1);
         XButton button2 = findViewById(R.id.XButton2);
+        XButton button4 = findViewById(R.id.XButton4);
 
         button1.setOnClickListener(v -> {
             Toast.makeText(this, "button1", Toast.LENGTH_LONG).show();
             button1.setDefaultColor(0xffff0000);
         });
         button2.setOnClickListener(v -> Toast.makeText(this, "button2", Toast.LENGTH_LONG).show());
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button4.requestLayout();
+                button4.invalidate();
+                handler.postDelayed(this, 15000);
+            }
+        }, 15000);
+
     }
 }
--- xbuttonlib/src/main/java/com/zhxh/xbuttonlib/XButton.java
+++ xbuttonlib/src/main/java/com/zhxh/xbuttonlib/XButton.java
@@ -1,11 +1,15 @@
 package com.zhxh.xbuttonlib;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.AppCompatButton;
@@ -44,6 +48,15 @@ public class XButton extends AppCompatButton {
 
     boolean isTouchPass = true;
 
+
+    //动画
+    boolean XisShaderAnim;
+
+    private int animatedValue;
+    private int colorEnd;
+    private int colorStart;
+    private int animatedValue1;
+
     public XButton(Context context) {
         this(context, null);
     }
@@ -68,6 +81,7 @@ public class XButton extends AppCompatButton {
         angleCorner = a.getDimensionPixelSize(R.styleable.XButton_XangleCorner, angleCorner);
         strokeWidth = a.getDimensionPixelSize(R.styleable.XButton_XstrokeWidth, strokeWidth);
         drawablePadding = a.getDimensionPixelSize(R.styleable.XButton_XdrawablePadding, drawablePadding);
+        XisShaderAnim = a.getBoolean(R.styleable.XButton_XisShaderAnim, XisShaderAnim);
 
         if (null == bounds) {
             bounds = new Rect();
@@ -80,11 +94,76 @@ public class XButton extends AppCompatButton {
         setDrawablePadding(drawablePadding);
         setBtnDrawable();
 
+        setAnim(XisShaderAnim);
+
         //设置按钮点击之后的颜色更换
         setOnTouchListener((arg0, event) -> {
             setBackgroundDrawable(gradientDrawable);
             return setColor(event.getAction());
         });
+    }
+
+    private void setAnim(boolean XisShaderAnim) {
+
+        this.XisShaderAnim = XisShaderAnim;
+
+        if (!XisShaderAnim) {
+            return;
+        }
+
+        postInvalidate();
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, 255);
+        animator.setDuration(10000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                animatedValue = (int) animation.getAnimatedValue();
+
+                if ((animatedValue < 255)) {
+                    colorStart = Color.rgb(255, animatedValue, 255 - animatedValue);
+                    colorEnd = Color.rgb(animatedValue, 0, 255 - animatedValue);
+
+                } else if (animatedValue == 255) {
+                    ValueAnimator animator1 = ValueAnimator.ofInt(0, 255);
+                    animator1.setDuration(2500);
+                    animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            animatedValue1 = (int) animation.getAnimatedValue();
+                            colorStart = Color.rgb(255 - animatedValue1, 255 - animatedValue1, animatedValue1);
+                            colorEnd = Color.rgb(255, 0, animatedValue1);
+
+                            if (animatedValue1 == 255) {
+                                ValueAnimator animator2 = ValueAnimator.ofInt(0, 255);
+                                animator2.setDuration(2500);
+                                animator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator animation) {
+                                        int animatedValue2 = (int) animation.getAnimatedValue();
+                                        colorStart = Color.rgb(animatedValue2, 0, 255);
+                                        colorEnd = Color.rgb(255 - animatedValue2, 0, 255);
+                                        invalidate();
+
+                                    }
+                                });
+                                animator2.start();
+                            }
+
+                            invalidate();
+
+                        }
+                    });
+                    animator1.start();
+                }
+
+                invalidate();
+            }
+        });
+
+        animator.start();
+
+        requestLayout();
     }
 
     @Override
@@ -182,6 +261,22 @@ public class XButton extends AppCompatButton {
         return isTouchPass;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (!XisShaderAnim) {
+            return;
+        }
+
+        int width = getWidth();
+        int height = getHeight();
+        Paint paint = new Paint();
+        LinearGradient backGradient = new LinearGradient(width, 0, 0, 0, new int[]{colorStart, colorEnd}, new float[]{0, 1f}, Shader.TileMode.CLAMP);
+        paint.setShader(backGradient);
+        canvas.drawRect(0, 0, width, height, paint);
+
+    }
 
     //对外定义接口
     public void setPressedColor(int pressedColor) {
