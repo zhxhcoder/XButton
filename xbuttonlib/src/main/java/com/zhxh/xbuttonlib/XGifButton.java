fe6ac32c0dd96418f02e1d3a63aa188c22e2e25b
zhxh <zhxhcoder@gmail.com>
2018-06-29 17:21:15 +0800

    XGifButton

--- xbuttonlib/src/main/java/com/zhxh/xbuttonlib/XGifButton.java
+++ xbuttonlib/src/main/java/com/zhxh/xbuttonlib/XGifButton.java
@@ -1,7 +1,13 @@
 package com.zhxh.xbuttonlib;
 
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+
+import java.io.IOException;
 
 import pl.droidsonroids.gif.GifTextView;
 
@@ -9,6 +15,10 @@ import pl.droidsonroids.gif.GifTextView;
  * Created by zhxh on 2018/6/29
  */
 public final class XGifButton extends GifTextView {
+
+    XGifDrawable gifDrawable;
+    boolean isAnimComplete;
+
     public XGifButton(Context context) {
         super(context);
     }
@@ -21,7 +31,48 @@ public final class XGifButton extends GifTextView {
         super(context, attrs, defStyle);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public XGifButton(Context context, AttributeSet attrs, int defStyle, int defStyleRes) {
         super(context, attrs, defStyle, defStyleRes);
     }
+
+
+    public XGifButton bindGifSource(int rGif) {
+
+        try {
+            gifDrawable = new XGifDrawable(getResources(), rGif);
+            return this;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+
+    public void setIsAnimComplete(boolean isAnimComplete) {
+
+        if (!isAnimComplete) {
+            setFirstFrame();
+        } else {
+            setLastFrame();
+        }
+    }
+
+    public void setFirstFrame() {
+        isAnimComplete = false;
+        gifDrawable.seekToFrameAndGet(0);
+        Drawable drawable = new BitmapDrawable(getResources(), gifDrawable.seekToFrameAndGet(0));
+        this.setBackground(drawable);
+        invalidate();
+    }
+
+    public void setLastFrame() {
+        isAnimComplete = true;
+        gifDrawable.stop();
+        Drawable drawable1 = new BitmapDrawable(getResources(), gifDrawable.seekToFrameAndGet(gifDrawable.getNumberOfFrames() - 1));
+        this.setBackground(drawable1);
+        this.setClickable(false);
+    }
+
+
 }
