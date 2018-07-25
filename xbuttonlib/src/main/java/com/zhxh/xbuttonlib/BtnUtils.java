80e0441eff7d7524415a8a80f70a7787e2d78b91
zhxh <zhxhcoder@gmail.com>
2018-07-25 11:07:51 +0800

    isQuickClick

--- xbuttonlib/src/main/java/com/zhxh/xbuttonlib/BtnUtils.java
+++ xbuttonlib/src/main/java/com/zhxh/xbuttonlib/BtnUtils.java
@@ -3,9 +3,10 @@ package com.zhxh.xbuttonlib;
 /**
  * Created by zhxh on 2018/7/25
  */
-public class BtnUtils {
+public final class BtnUtils {
 
     private static long lastClickTime;
+
     public synchronized static boolean isQuickClick() {
         long time = System.currentTimeMillis();
         if (time - lastClickTime < 500) {
