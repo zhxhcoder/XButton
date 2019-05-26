2ea50cf6915b8a3af79ea68d8b7c79a135e52e19
zhxh <zhxhcoder@gmail.com>
2019-05-26 12:04:17 +0800

    引入kotlin

---
+++ app/src/main/java/com/zhxh/xbutton/dummy/ChartData.kt
@@ -0,0 +1,31 @@
+package com.zhxh.xbutton.dummy
+
+import com.google.gson.annotations.SerializedName
+import com.zhxh.xchartlib.entity.IAxisValue
+
+/**
+ * Created by zhxh on 2018/6/28
+ */
+class ChartData(@field:SerializedName(value = "date", alternate = ["logDay"])
+                var date: String?, @field:SerializedName(value = "value", alternate = ["yield"])
+                var value: String?) : IAxisValue {
+
+    @SerializedName(value = "name", alternate = ["UserName"])
+    var name: String? = null
+    @SerializedName(value = "list", alternate = ["DayInitsAsstes"])
+    var list: List<ChartData>? = null
+
+    var isFollow: Boolean = false
+
+    override fun xValue(): String? {
+        return date
+    }
+
+    override fun yValue(): Float {
+        return java.lang.Float.parseFloat(value!!)
+    }
+
+    override fun flagValue(): Int {
+        return 0
+    }
+}
---
+++ app/src/main/java/com/zhxh/xbutton/dummy/PostTestEvent.kt
@@ -0,0 +1,6 @@
+package com.zhxh.xbutton.dummy
+
+/**
+ * Created by zhxh on 2019/1/4
+ */
+class PostTestEvent(var testMsg: String?)
